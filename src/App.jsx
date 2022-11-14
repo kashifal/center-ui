19c20aee9a668bf840a43afa45e1dff686f542a3
Usama Arslan <usama.arslan@softpyramid.com>
2022-11-14 21:35:02 +0500

    code chuks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
+import React from 'react';
+import Code from './components/Code/code';
+
+const App = () => {
+  const codeString = `
+ <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite + React</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
+  `;
+  const p = `<p>Hello P</p>`;
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Code code={codeString} language="html" />
+      <Code code={p} language="html" />
+
+  
+    </div>
   )
 }
+
+export default App
