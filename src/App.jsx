05c94d56f3bb5ead471aa9d4c6fe7f3a1a2adf3d
Usama Arslan <usama.arslan@softpyramid.com>
2022-11-17 15:56:02 +0500

    layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import Code from './components/Code/code';
+import Header from './components/Header/Header';
+import About from './routes/About';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/Home/Home';
+import Layout from './components/Sidebar/Layout';
+
 
 const App = () => {
-  const codeString = `
- <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vite + React</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.jsx"></script>
-  </body>
-</html>
-  `;
-  const p = `<p>Hello P</p>`;
   return (
-    <div>
-      <Code code={codeString} language="html" />
-      <Code code={p} language="html" />
+    <Fragment> 
+
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Route>
+      
+      </Routes> 
+   
 
   
-    </div>
+    </Fragment>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css';
+import './App';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>
 )
