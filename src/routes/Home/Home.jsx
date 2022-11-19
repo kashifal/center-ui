3b95a0dd1d62a48f9337065934325c65e5eb2e9f
Usama Arslan <usama.arslan@softpyramid.com>
2022-11-19 14:37:47 +0500

    fontFamily added

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1 className='text-5xl font-black'>Home Page</h1>
+      <p>cool</p>
+    </div>
   )
 }
 
