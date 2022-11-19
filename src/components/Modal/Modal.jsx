8d5c904dba9421c9f23877fe669ed57ee25641bf
Usama Arslan <usama.arslan@softpyramid.com>
2022-11-19 17:24:06 +0500

    logic add

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Modal() {
+  const isOpen = useSelector((state) =>  state.ModalSlice.isOpen);
   return (
-    <div className='absolute top-[100px] left-[400px] bg-white h-[400px] w-[600px] rounded p-5 flex flex-col'>
-      <div className='max-h-fit border-b hover:border-primary px-3 py-2 flex items-center gap-4'>
+    <div className={`absolute ml-auto mr-auto left-0 right-0 top-0 ${isOpen ? 'flex' : 'hidden'}  bottom-0 mt-16 mb-auto bg-white h-[400px] w-[750px] rounded-lg p-5  flex-col`}>
+      <div className='w-full border-b hover:border-primary  py-2 flex items-center gap-4'>
         <label htmlFor='search-input'>
           <svg
             xmlns='http://www.w3.org/2000/svg'
