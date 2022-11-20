2e2063f50c44eea7a19e049d9fa9afe09017e7d2
Usama Arslan <usama.arslan@softpyramid.com>
2022-11-20 12:59:59 +0500

    small change

---
+++ src/components/Silder/Slider.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const Slider = () => {
+  return (
+    <Fragment>
+        <div className='flex items-center gap-x-4 h-52 w-full px-2  justify-start flex-nowrap overflow-x-scroll'>
+             
+          
+        </div>
+    </Fragment>
+    
+  )
+}
+
+export default Slider
